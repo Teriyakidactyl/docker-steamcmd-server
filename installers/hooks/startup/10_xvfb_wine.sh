2b3aeea1f3dd0e3c3cce25bc021fdda599d3efd2
Zuckuss <ePQ1b4gnPZlIZ4M2VmHJ@gmail.com>
2025-04-28 13:32:23 -0700

    update 10_xvfb_wine.sh

--- installers/hooks/startup/10_xvfb_wine.sh
+++ installers/hooks/startup/10_xvfb_wine.sh
@@ -2,12 +2,8 @@
 
 log "Creating $APP_NAME xvfb-run APP_COMMAND"
 
-export APP_COMMAND=(
-xvfb-run \
+# xvfb-run MAN: https://linux.die.net/man/1/xvfb
+
+export APP_COMMAND="xvfb-run \
 --auto-servernum \
---server-args='-screen 0 640x480x24:32 -nolisten tcp' $APP_COMMAND_PREFIX $APP_FILES/$APP_EXE $APP_ARGS \
--nosteamclient \
--game \
--server \
--log
-)
+--server-args='-screen 0 640x480x24:32 -nolisten tcp' $APP_COMMAND_PREFIX $APP_FILES/$APP_EXE $APP_ARGS"

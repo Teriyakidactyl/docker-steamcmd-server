25208e60a87c9a36e0971e03f7a1dac27dc656d3
Zuckuss <ePQ1b4gnPZlIZ4M2VmHJ@gmail.com>
2025-04-27 19:34:13 -0700

    feat: create 20_wine.sh

---
+++ scripts/container/hooks/pre-startup/20_wine.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# https://wiki.winehq.org/FAQ#Is_there_a_64_bit_Wine , https://wiki.winehq.org/FAQ#How_do_I_create_a_32_bit_wineprefix_on_a_64_bit_system?
+# It requires the installation of 32 bit libraries in order to run 32 bit Windows applications
+
+# Check first time wine run, this will force Wine config creation so that our server load won't fail on first run.
+if [ -n "$WINEPREFIX" ] && [ ! -d "$WINEPREFIX" ]; then
+    # https://wiki.winehq.org/Wineboot
+    log "Wine first run detected, wait 15 seconds for wine config creation."
+    $APP_COMMAND_PREFIX wineboot -iuf | log_stdout
+fi
